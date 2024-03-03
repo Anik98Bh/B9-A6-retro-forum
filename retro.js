8ffe987830f0783f53f02c3ac3d293b9096d2a46
Anik98B <anikbhowmik0188@gmail.com>
2024-03-03 17:43:28 +0600

    complite letest post section

--- retro.js
+++ retro.js
@@ -4,17 +4,103 @@ const loadData = async () => {
     const posts = data.posts;
     // console.log(posts)
     displayPost(posts)
+    const response = await fetch('https://openapi.programming-hero.com/api/retro-forum/latest-posts');
+    const cards = await response.json();
+    // console.log(cards)
+    displayCard(cards)
 }
 
 const displayPost = (posts) => {
+
+    const postContainer = document.getElementById('Post-container');
+
     posts.forEach(post => {
-        console.log(post)
+        // console.log(post)
         const postCard = document.createElement('div');
-        postCard.classList = `card w-96 bg-[#F3F3F5] shadow-xl`;
-        postCard.innerHTML=`
-        
-        `
+        postCard.classList = `card w-3/4 bg-[#797DFC1A] shadow-xl my-4`;
+        postCard.innerHTML = `
+        <div class=" lg:flex gap-2 px-12 py-10">
+            <div class="avatar">
+                <div class="w-24 h-24 rounded-full">
+                    <img src="${post.image}" />
+                </div>
+            </div>
+            <div class="card-body">
+                <div class="flex font-medium">
+                    <p># ${post.category}</p>
+                    <p>Author : ${post.author.name}</p>
+                </div>
+                <h2 class="card-title">${post.title}</h2>
+                <p>${post.description}</p>
+
+                <hr class=" border-dashed bg-[#12132D] my-5">
+
+                <div class="grid lg:flex justify-between items-center">
+                    <div class="flex items-center gap-10">
+                        <div class="flex gap-2">
+                            <img src="./images/Group 13.svg" alt="">
+                            <p>${post.comment_count}</p>
+                        </div>
+                        <div class="flex gap-2">
+                            <img src="./images/Group 16.svg" alt="">
+                            <p>${post.view_count}</p>
+                        </div>
+                        <div class="flex gap-2">
+                            <img src="./images/Group 18.svg" alt="">
+                            <p>${post.posted_time} min</p>
+                        </div>
+                    </div>
+                    <div>
+                        <button class="btn btn-circle border-none">
+                        <img src="./images/Group 40106.png" alt="">
+                        </button>
+                    </div>
+                </div>
+                
+            </div>     
+        </div>
+        `;
+        postContainer.appendChild(postCard);
     });
+}
+
+const displayCard = (cards) => {
+    // console.log(cards)
+    const cardContainer = document.getElementById('card-container');
+
+    cards.forEach(key => {
+        console.log(key)
+        const cardSlort = document.createElement('div');
+        cardSlort.classList = `card-body bg-[#797DFC1A] shadow-xl rounded-3xl`;
+        cardSlort.innerHTML = `
+        <div>
+            <div class="">
+                <img class="rounded-3xl"
+                src="${key.cover_image}" alt="" />
+            </div>
+            <div class="flex gap-2 my-5">
+                <img src="./images/boxx.svg" alt="">
+                <p>${key.author.posted_date}</p>
+            </div>
+            <div class="">
+                <h2 class="card-title mb-2">${key.title}</h2>
+                <p>${key.description}</p>
+                <div class="flex gap-4 mt-5">
+                    <div class="avatar">
+                        <div class="w-11 h-11 rounded-full">
+                         <img src="${key.profile_image}" />
+                        </div>
+                    </div>
+                    <div>
+                        <h1 class="font-bold">${key.author.name}</h1>
+                        <p>${key.author.designation}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        `;
+        cardContainer.appendChild(cardSlort);
+    })
 }
 
 
